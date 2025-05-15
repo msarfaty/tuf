38a8644eb0840a01852d4d0949f4aed1589f3860
Mike Sarfaty <mjsarfaty@gmail.com>
2025-05-15 00:19:57 -0400

    test progress

--- pkg/state/workspace.go
+++ pkg/state/workspace.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 
 	"github.com/google/uuid"
 	"mikesarfaty.com/tuf/pkg/file"
@@ -22,40 +23,70 @@ type Workspace struct {
 	files   []*WorkspaceFile `yaml:"files"`
 }
 
-type Workspaces struct {
+type WorkspaceMgr struct {
 	workspaces []*Workspace `yaml:"workspaces"`
 }
 
-func (wss *Workspaces) AddWorkspace(path string) (*Workspace, error) {
-	ret := Workspace{}
+func (wsmgr *WorkspaceMgr) String() string {
+	workspaces := []string{}
+	for _, ws := range wsmgr.workspaces {
+		workspaces = append(workspaces, fmt.Sprintf("%v", ws))
+	}
+
+	return fmt.Sprintf("WorkspaceMgr{workspaces=[%s]}", strings.Join(workspaces, ", "))
+}
+
+func (ws *Workspace) String() string {
+	var sb strings.Builder
+	sb.WriteString("Workspace{")
+	sb.WriteString(fmt.Sprintf("uuid=%s abspath=%s", ws.uuid, ws.abspath))
+	files := []string{}
+	for _, f := range ws.files {
+		files = append(files, fmt.Sprintf("%v", f))
+	}
+	sb.WriteString(fmt.Sprintf(" files=[%s]", strings.Join(files, ", ")))
+	sb.WriteString("}")
+	return sb.String()
+}
+
+func (wsf *WorkspaceFile) String() string {
+	return fmt.Sprintf("name=%s md5=%s", wsf.name, wsf.md5)
+}
+
+// Add a workspace to the workspaces
+func (wsmgr *WorkspaceMgr) AddWorkspace(path string) error {
+	fmt.Printf("Wsmgr has %d workspaces", len(wsmgr.workspaces))
+	ws := Workspace{}
 
 	abspath, err := filepath.Abs(path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get absolute path for %s: %w", path, err)
+		return fmt.Errorf("failed to get absolute path for %s: %w", path, err)
 	}
 	stat, err := os.Stat(abspath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to stat %s: %w", abspath, err)
+		return fmt.Errorf("failed to stat %s: %w", abspath, err)
 	}
 	if !stat.IsDir() {
-		return nil, fmt.Errorf("non-directory workspace provided (%s)", abspath)
+		return fmt.Errorf("non-directory workspace provided (%s)", abspath)
 	}
-	ret.abspath = abspath
+	ws.abspath = abspath
 
-	md5s, err := md5ForTerraformFiles(ret.abspath)
+	md5s, err := md5ForTerraformFiles(ws.abspath)
 	if err != nil {
-		return nil, fmt.Errorf("generating md5 for terraform files: %w", err)
+		return fmt.Errorf("generating md5 for terraform files: %w", err)
 	}
-	ret.files = []*WorkspaceFile{}
+	ws.files = []*WorkspaceFile{}
 	for terraformFilePath, md5 := range md5s {
-		ret.files = append(ret.files, &WorkspaceFile{
+		ws.files = append(ws.files, &WorkspaceFile{
 			name: filepath.Base(terraformFilePath),
 			md5:  md5,
 		})
 	}
 
-	ret.uuid = uuid.NewString()
-	return &ret, nil
+	ws.uuid = uuid.NewString()
+	wsmgr.workspaces = append(wsmgr.workspaces, &ws)
+	fmt.Printf("Wsmgr has %d workspaces", len(wsmgr.workspaces))
+	return nil
 }
 
 // validates a workspace to ensure that the stored state of the workspace matches the current working state of the workspace
@@ -82,7 +113,7 @@ func (ws *Workspace) Validate() error {
 }
 
 // the culmination of multiple workspace validations
-func (wss *Workspaces) Validate() error {
+func (wss *WorkspaceMgr) Validate() error {
 	errs := []error{}
 
 	for _, ws := range wss.workspaces {
@@ -93,6 +124,13 @@ func (wss *Workspaces) Validate() error {
 	}
 
 	return errors.Join(errs...)
+}
+
+// yields a new workspaces object with no workspaces
+func NewWorkspaceMgr() *WorkspaceMgr {
+	return &WorkspaceMgr{
+		workspaces: []*Workspace{},
+	}
 }
 
 // finds all terraform files in a directory and generates their md5s, returning the mapping from abspath:md5
